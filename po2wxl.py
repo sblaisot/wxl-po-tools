5c0b39d7d50b45fbe01453c5e069bc25c16c97d3
Sébastien BLAISOT <sebastien@blaisot.org>
2016-01-09 21:16:03 +0100

    po2wxl.py: add -l:,--langid= option to auto add LCID string based on Language metadata

--- po2wxl.py
+++ po2wxl.py
@@ -6,17 +6,63 @@ import sys
 reload(sys)  
 sys.setdefaultencoding('utf8')
 
-import os;
-import polib;
+import getopt
+import polib
 from lcid import LCIDs;
 
+def version():
+    print "po2wxl.py version 0.1\n"
 
-if len(sys.argv) <= 2:
-    print "Usage: wxl2pot.py <wxl file> <pot file>";
-    os._exit(1);
+def help():
+    print textwrap.dedent("""
+      Usage: po2wxl.py [OPTION]... PO_SOURCE_FILE WXL_DEST_FILE
+      Transform the file PO_SOURCE_FILE in po format into a wxl file WXL_DEST_FILE
+      Example: po2wxl.py -l LangId en-us.po en-us.wxl
 
-sourcefile = sys.argv[1];
-destfile = sys.argv[2];
+      Options:
+        -h, --help:            print this help message and exit
+        -V, --version          print version information and exit
+        -l, --langid=LANGID    automatically determine LCID based on language and 
+                               add a string with id LANGID containing the LCID
+""")
+
+def usage():
+    print textwrap.dedent("""\
+      Usage: po2wxl.py [OPTION]... PO_SOURCE_FILE WXL_DEST_FILE
+      Try 'po2wxl.py --help' for more information.
+    """)
+
+
+# Main
+langid = ""
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hVl:", ["help", "version", "langid="])
+except getopt.GetoptError as err:
+    # print help information and exit:
+    print str(err) # will print something like "option -a not recognized"
+    usage()
+    sys.exit(2)
+output = None
+verbose = False
+for o, a in opts:
+    if o in ("-V", "--version"):
+        version()
+        sys.exit()
+    elif o in ("-h", "--help"):
+        help()
+        sys.exit()
+    elif o in ("-l", "--langid"):
+        langid = a
+    else:
+        assert False, "unhandled option"
+
+
+if len(args) < 2:
+    usage()
+    sys.exit(1)
+
+sourcefile = args[0];
+destfile = args[1];
 
 po = polib.pofile(sourcefile);
 
@@ -29,17 +75,25 @@ language = [value for name, value in metadata if name == "Language"]
 
 culture = language[0].lower().replace('_','-');
 codepage = LCIDs[culture]['codepage'];
-langId = LCIDs[culture]['LCID'];
+langIdAuto = LCIDs[culture]['LCID'];
 
 f = open(destfile,'w');
 f.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
 f.write("<WixLocalization Culture=\"" + culture + "\" Codepage=\"" + str(codepage) + "\"\n");
 f.write("                 xmlns=\"http://schemas.microsoft.com/wix/2006/localization\">\n");
 f.write("\n");
-f.write("<!-- This wxl file has been auto generated from a po file -->\n");
-f.write("<!-- using https://github.com/sblaisot/wxl-po-tools -->\n");
-f.write("<!-- Source File: " + sourcefile + " -->\n");
+f.write("  <!-- ---------------------------------------------------- -->\n");
+f.write("  <!-- This wxl file has been auto generated from a po file -->\n");
+f.write("  <!-- using https://github.com/sblaisot/wxl-po-tools       -->\n");
+f.write("  <!-- Source File: " + sourcefile.ljust(39) + " -->\n");
+f.write("  <!-- ---------------------------------------------------- -->\n");
 f.write("\n");
+
+if langid:
+    f.write("  <!-- This contains the LangID and should be translated to reflect the correct LangID. -->\n")
+    f.write("  <!-- Supported language and codepage codes can be found here: https://msdn.microsoft.com/en-us/goglobal/bb964664.aspx -->\n")
+    f.write("  <String Id=\"" + langid + "\">" + str(langIdAuto) + "</String>\n")
+    f.write("\n")
 
 for entry in po:
     if entry.comment != "":
