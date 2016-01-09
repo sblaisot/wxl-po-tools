19bc4b84ae5b49ebb6bda799142311a2f7888387
Sébastien BLAISOT <sebastien@blaisot.org>
2016-01-09 19:48:25 +0100

    wxl2pot.py: add -l:,--langid= option to ignore LCID string

--- wxl2pot.py
+++ wxl2pot.py
@@ -1,46 +1,94 @@
 #!/usr/bin/python
 
-import sys;
-import os;
-from xml.dom import minidom;
-import polib;
+import getopt, sys
+import textwrap
+from xml.dom import minidom
+import polib
+
+def version():
+    print "wxl2pot.py version 0.1\n"
+
+def help():
+    print textwrap.dedent("""
+      Usage: wxl2pot.py [OPTION]... WXL_SOURCE_FILE POT_DEST_FILE
+      Transform the file WXL_SOURCE_FILE in wxl format into a pot file POT_DEST_FILE
+      Example: wxl2pot.py -l LangId en-us.wxl en-us.pot
+      
+      Options:
+        -h, --help:            print this help message and exit
+        -V, --version          print version information and exit
+        -l, --langid=LANGID    ignore string with Id LANGID containing the LCID
+""")
+
+def usage():
+    print textwrap.dedent("""\
+      Usage: wxl2pot.py [OPTION]... WXL_SOURCE_FILE POT_DEST_FILE
+      Try 'wxl2pot.py --help' for more information.
+    """)
 
 
-if len(sys.argv) <= 2:
-    print "Usage: wxl2pot.py <wxl file> <pot file>";
-    os._exit(1);
+# Main
 
-sourcefile = sys.argv[1];
-destfile = sys.argv[2];
+langid = ""
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hVl:", ["help", "version", "langid="])
+except getopt.GetoptError as err:
+    # print help information and exit:
+    print str(err) # will print something like "option -a not recognized"
+    usage()
+    sys.exit(2)
+output = None
+verbose = False
+for o, a in opts:
+    if o in ("-V", "--version"):
+        version()
+        sys.exit()
+    elif o in ("-h", "--help"):
+        help()
+        sys.exit()
+    elif o in ("-l", "--langid"):
+        langid = a
+    else:
+        assert False, "unhandled option"
 
-doc = minidom.parse(sourcefile);
+if len(args) < 2:
+    usage()
+    sys.exit(1)
 
-wixloc = doc.getElementsByTagName("WixLocalization")[0];
-culture = wixloc.getAttribute("Culture");
-codepage = wixloc.getAttribute("Codepage");
+sourcefile = args[0]
+destfile = args[1]
 
-po = polib.POFile(wrapwidth=0);
+doc = minidom.parse(sourcefile)
+
+wixloc = doc.getElementsByTagName("WixLocalization")[0]
+culture = wixloc.getAttribute("Culture")
+codepage = wixloc.getAttribute("Codepage")
+
+po = polib.POFile(wrapwidth=0)
 po.metadata = {
     'MIME-Version': '1.0',
     'Content-Type': 'text/plain; charset=utf-8',
     'Content-Transfer-Encoding': '8bit',
 }
 
-root = doc.documentElement;
-nodes = root.childNodes;
+root = doc.documentElement
+nodes = root.childNodes
 
-comment = "";
+comment = ""
 
 for node in nodes:
     if node.nodeType == node.COMMENT_NODE:
         if not comment:
-            comment = node.data;
+            comment = node.data
         else:
             comment = comment+ "\n" + node.data
     if node.nodeType == node.ELEMENT_NODE:
         if node.tagName == "String":
-            stringId = node.getAttribute("Id");
-            stringContent = node.firstChild.data;
+            stringId = node.getAttribute("Id")
+            if stringId == langid:
+                continue
+
+            stringContent = node.firstChild.data
             entry = polib.POEntry(
                 comment = comment,
                 msgctxt = stringId,
@@ -48,5 +96,5 @@ for node in nodes:
             )
             po.append(entry)
             if comment != "":
-                comment = "";
-po.save(destfile);
+                comment = ""
+po.save(destfile)
